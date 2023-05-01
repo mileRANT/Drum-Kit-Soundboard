84fd475fdd8cf4ef125712ecb592cce05253add7
mileRANT <liem.htran@outlook.com>
2023-05-01 16:05:34 -0400

    switch statement for adding in the sounds, adding in background image url for the buttons

--- index.js
+++ index.js
@@ -2,10 +2,60 @@
 
 for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-    
+
 }
 function handleClick(){
-    alert("I got clicked");
+    
+
+    //we can determine what was clicked by using the this object
+// in this way, we can identify and provide the appropriate sound clip
+    // this.style.color = "white";
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML){
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");   
+            audio.play();  
+            break;
+
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3");     
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3");     
+            audio.play();
+            break;            
+
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");     
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("./sounds/snare.mp3");     
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("./sounds/crash.mp3");     
+            audio.play();
+            break;
+        
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");     
+            audio.play();
+            break;
+
+        default:
+            console.log(buttonInnerHTML)
+    }
+
+
+    // var audio = new Audio("./sounds/tom-1.mp3");
+    // audio.play();
+
+
 }
 
 //anonymous function example
@@ -13,3 +63,16 @@ function handleClick(){
 // function (){
 //     alert("I got clicked");
 // });
+
+
+// // general learning about functions as inputs
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// function multiply(num1, num2){
+//     return num1*num2;
+// }
+
+// function calculator(num1, num2, operator){
+//     return operator(num1,num2);
+// }
