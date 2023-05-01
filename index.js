f57017c4787dc8ad41e815ba1798c512b50c38a7
mileRANT <liem.htran@outlook.com>
2023-05-01 15:40:21 -0400

    Eventlistener to all buttons

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-document.querySelector("button").addEventListener("click", handleClick);
+// document.querySelector("button").addEventListener("click", handleClick);
 
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    
+}
 function handleClick(){
     alert("I got clicked");
 }
+
+//anonymous function example
+// document.querySelector("button").addEventListener("click", 
+// function (){
+//     alert("I got clicked");
+// });
